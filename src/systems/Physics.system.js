8b04dfbd9b4dbcdd5e0e5f6d71d0ffdd9b70f98a
Noman5237 <anonyman637@gmail.com>
2022-04-20 04:27:27 +0600

    falling is working

--- src/systems/Physics.system.js
+++ src/systems/Physics.system.js
@@ -1,16 +1,39 @@
+import { Composite } from 'matter-js';
 import { Query, System } from 'miaam-ecs';
-import { Box, Position } from '../components/index.js';
+import { Box, Position, World } from '../components/index.js';
 import { PhysicsManager } from '../manager/index.js';
 
 class PhysicsSystem extends System {
 	constructor() {
 		super();
 
+		this.queries.world = new Query({
+			components: {
+				and: [World],
+			},
+		});
+
 		this.queries.bodies = new Query({
 			components: {
 				and: [Box, Position],
 			},
 		});
+	}
+
+	init({ components, entities }) {
+		const bodiesComposite = Composite.create();
+		const bodies = this.queries.bodies.run({ components, entities });
+		bodies.forEach((entity) => {
+			const { body } = entity[Box.name].props;
+			Composite.add(bodiesComposite, body);
+		});
+
+		const worldComp = this.queries.world.run({ components, entities })[0][World.name];
+		const worldComposite = worldComp.props.composite;
+		Composite.add(worldComposite, bodiesComposite);
+
+		PhysicsManager.instance.engine.world = worldComposite;
+		PhysicsManager.instance.engine.gravity.y = 0.5;
 	}
 
 	update({ delta, components, entities }) {
@@ -20,6 +43,7 @@ class PhysicsSystem extends System {
 			const box = entity[Box.name];
 			const position = entity[Position.name];
 			position.update(box.position);
+			console.log(box.position);
 		});
 	}
 }
