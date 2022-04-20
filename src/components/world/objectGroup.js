826681c61c2e9c56f8cc3cce04b93d35789ea040
Noman5237 <anonyman637@gmail.com>
2022-04-20 06:12:36 +0600

    npm publish

--- src/components/world/objectGroup.js
+++ src/components/world/objectGroup.js
@@ -11,37 +11,42 @@ class ObjectGroup extends Layer {
 		objectsArray.forEach((object) => {
 			object.properties = constructPropertiesMap(object.properties);
 			objectsMap[object.name] = object;
-			if (this.properties.physics !== true) {
-				return;
-			}
-			const { x, y, width, height } = object;
-			const { isStatic, isSensor } = this.properties;
-			const body = Bodies.rectangle(x + (width - map.tilewidth) / 2, y + (height - map.tileheight) / 2, width, height, {
-				label: object.name,
-				isStatic,
-			});
-			if (isSensor) {
-				Body.set(body, 'isSensor', true);
-				Events.on(PhysicsManager.instance.engine, 'collisionStart', (event) => {
-					const pairs = event.pairs[0];
-					if (pairs.bodyA.label === object.name) {
-						const eventTrigger = new CustomEvent(`collisionStart.${object.name}`, { detail: event });
-						// setTimeout(() => {
-						PhysicsManager.instance.events.dispatchEvent(eventTrigger);
-						// }, 0);
+			if (this.properties.physics === true) {
+				const { x, y, width, height } = object;
+				const { isStatic, isSensor } = this.properties;
+				const body = Bodies.rectangle(
+					x + (width - map.tilewidth) / 2,
+					y + (height - map.tileheight) / 2,
+					width,
+					height,
+					{
+						label: object.name,
+						isStatic,
 					}
-				});
-				Events.on(PhysicsManager.instance.engine, 'collisionEnd', (event) => {
-					const pairs = event.pairs[0];
-					if (pairs.bodyA.label === object.name) {
-						const eventTrigger = new CustomEvent(`collisionEnd.${object.name}`, { detail: event });
-						// setTimeout(() => {
-						PhysicsManager.instance.events.dispatchEvent(eventTrigger);
-						// }, 0);
-					}
-				});
+				);
+				if (isSensor) {
+					Body.set(body, 'isSensor', true);
+					Events.on(PhysicsManager.instance.engine, 'collisionStart', (event) => {
+						const pairs = event.pairs[0];
+						if (pairs.bodyA.label === object.name) {
+							const eventTrigger = new CustomEvent(`collisionStart.${object.name}`, { detail: event });
+							// setTimeout(() => {
+							PhysicsManager.instance.events.dispatchEvent(eventTrigger);
+							// }, 0);
+						}
+					});
+					Events.on(PhysicsManager.instance.engine, 'collisionEnd', (event) => {
+						const pairs = event.pairs[0];
+						if (pairs.bodyA.label === object.name) {
+							const eventTrigger = new CustomEvent(`collisionEnd.${object.name}`, { detail: event });
+							// setTimeout(() => {
+							PhysicsManager.instance.events.dispatchEvent(eventTrigger);
+							// }, 0);
+						}
+					});
+				}
+				Composite.add(this.composite, body);
 			}
-			Composite.add(this.composite, body);
 		});
 
 		this.data.objects = objectsMap;
