465bf0b8acf8891a7c33107c2e81d37c55b7dbfe
Noman5237 <anonyman637@gmail.com>
2022-04-20 07:34:02 +0600

    audio done

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import * as Components from './src/components/index.js';
 import * as Entities from './src/entities/index.js';
 import * as Systems from './src/systems/index.js';
 import Keyboard from './src/input/keyboard/keyboard.js';
+import Audio from './src/audio/Audio.js';
 
 import { AssetsManager, GameManager, SceneManager, PhysicsManager } from './src/manager/index.js';
 
@@ -25,4 +26,5 @@ export {
 	Composites,
 	Collision,
 	Body,
+	Audio,
 };
---
+++ src/audio/Audio.js
@@ -0,0 +1,60 @@
+import { Sound } from '@pixi/sound';
+
+class Audio {
+	clip;
+
+	#url;
+
+	#volume;
+
+	#loop;
+
+	constructor({ url, loop }) {
+		this.#url = url;
+		if (loop) this.createClip({ url, loop });
+		else this.createClip({ url });
+		this.#volume = this.clip.volume;
+		this.#loop = this.clip.loop;
+	}
+
+	createClip = ({ url, loop }) => {
+		this.clip = Sound.from({
+			url,
+			preload: true,
+		});
+		if (loop) {
+			this.#loop = loop;
+		}
+		console.log(this.#loop);
+	};
+
+	play = () => {
+		this.clip.play();
+	};
+
+	pause = () => {
+		this.clip.pause();
+	};
+
+	resume = () => {
+		this.clip.resume();
+	};
+
+	destroy = () => {
+		this.clip.destroy();
+	};
+
+	get volume() {
+		return this.#volume;
+	}
+
+	set volume(volume) {
+		this.#volume = volume;
+	}
+
+	set loop(loop) {
+		this.#loop = loop;
+	}
+}
+
+export default Audio;
